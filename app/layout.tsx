b9d55add8efca1355b794f78ab3232c566a4a3c9
imsofiaescalona <111590348+imsofiaescalona@users.noreply.github.com>
2025-11-12 21:17:16 -0700

    Update metadata for Nutrition Guardian AI

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ import { SessionProvider } from "next-auth/react";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://chat.vercel.ai"),
-  title: "Next.js Chatbot Template",
-  description: "Next.js chatbot template using the AI SDK.",
+  title: "Nutrition Guardian AI",
+  description: "Helpful Chatbot for Meat Science Students",
 };
 
 export const viewport = {
